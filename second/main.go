428304f2f8873b78a9a6eddffb2b2cfcd454615c
Cherry-meat <olaboltikova4@gmail.com>
2025-10-12 16:30:19 +0300

    add comments

--- second/main.go
+++ second/main.go
@@ -11,37 +11,42 @@ func main() {
 	
 	n := 1000
 	sequence := make([]int, n)
+	// Заполняем массив случайными числами от 0 до 10000
 	for i := 0; i < n; i++ {
 		sequence[i] = rand.Intn(10001) 
 	}
 
-	max14 := -1   
-	max7 := -1    
-	max2 := -1    
-	max := -1     
+	max14 := -1   // максимальное число, кратное 14
+	max7 := -1    // максимальное число, кратное 7 (но не 14)
+	max2 := -1    // максимальное число, кратное 2 (но не 14)
+	max := -1     // максимальное число в последовательности
 
+	// Проходим по всем числам последовательности
 	for _, num := range sequence {
 		if num%14 == 0 {
 			if num > max14 {
 				max14 = num
 			}
-		} else if num%7 == 0 && num&14 != 0 {
+		} else if num%7 == 0 {
 			if num > max7 {
 				max7 = num
 			}
-		} else if num%2 == 0 && num&14 != 0 {
+		} else if num%2 == 0 {
 			if num > max2 {
 				max2 = num
 			}
 		}
 		
+		// Обновляем абсолютный максимум
 		if num > max {
 			max = num
 		}
 	}
-	if max * max14 > max2 * max7{
-		fmt.Printf("%d * %d = %d", max,max14,max*max14)
-	}else{
-		fmt.Printf("%d * %d = %d", max2,max7,max2*max7)
+
+	// Выбираем максимальное произведение
+	if max * max14 > max2 * max7 {
+		fmt.Printf("%d * %d = %d", max, max14, max*max14)
+	} else {
+		fmt.Printf("%d * %d = %d", max2, max7, max2*max7)
 	}
 }
