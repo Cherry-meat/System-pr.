8960eece2ec4b7953226f620cbe8290ea46ae21d
Cherry-meat <olaboltikova4@gmail.com>
2025-10-12 16:26:24 +0300

    add comments

--- lr3 - cherepaxa i zaic/main.go
+++ lr3 - cherepaxa i zaic/main.go
@@ -5,32 +5,34 @@ import(
 	"time"
 )
 
+// ch - канал для передачи сообщений о завершении забега
 var ch = make(chan string)
 
 type animal struct{
-	name string
-	speed int
+	name string  
+	speed int    
 }
 
+// run - метод, имитирующий бег животного
 func (an animal) run (){
-
+	// Цикл бега на 50 метров
 	for i := 0; i < 50; i++ {
-
 		time.Sleep(time.Duration(an.speed) * time.Millisecond)
 		fmt.Println("Животное", an.name ,"пробежал", i, " метров.")
-
 	}
 
+	// Отправка имени животного в канал по завершении забега
 	ch <- an.name
 }
 
 func main(){
-
 	var rabbit animal = animal{"Заяц", 10}
 	var turtle animal = animal{"Черепаха", 10}
 
 	go rabbit.run() 
 	go turtle.run()
 	
+	// Ожидание и вывод имени первого животного, завершившего забег
 	fmt.Println(<- ch)
+
 }
